7bbe1b6f63454b3f6a0328b4ed29f957bff9b1f5
anandh86 <anandh86@gmail.com>
2019-09-25 22:06:45 +0800

    Updated board

--- Chess/ChessBoard.cpp
+++ Chess/ChessBoard.cpp
@@ -13,22 +13,16 @@ void ChessBoard::vDisplayBoard(void)
 	for (int i = 8; i > 0; i--)
 	{
 		SetConsoleTextAttribute(console, 15); //Black background
-		cout << "\t ";
-
-		SetConsoleTextAttribute(console, 240); //White background black text
-		//cout << "#################################\n"; //Border
-		cout << "                                 \n"; //Border
-
-		SetConsoleTextAttribute(console, 15); //Black background
-		cout << "\t";
+		cout << "\t ";		
 
 		SetConsoleTextAttribute(console, 15); //White background black text
 		cout << i;
 
+		SetConsoleTextAttribute(console, 240); //White background black text
+		cout << "  ";//Border
+
 		for (int j = 1; j < 9; j++)
 		{
-			SetConsoleTextAttribute(console, 240); //White background black text
-			cout << " "; //Border
 
 			if (count == 0)
 			{
@@ -48,19 +42,19 @@ void ChessBoard::vDisplayBoard(void)
 		else count = 0;
 
 		SetConsoleTextAttribute(console, 240); //White background black text
-		cout << " " << endl;//Border
+		cout << "  " << endl;//Border
 	}
 
 	SetConsoleTextAttribute(console, 15); //Black background
-	cout << "\t ";
+	cout << "\t  ";
 
 	SetConsoleTextAttribute(console, 240); //White background black text
-	cout << "                                 \n";
+	cout << "                            \n";
 
 	SetConsoleTextAttribute(console, 15); //Black background
 	cout << "\t";
 
-	cout << "   A   B   C   D   E   F   G   H  \n";
+	cout << "     A  B  C  D  E  F  G  H  \n";
 	//cout << " ##A###B###C###D###E###F###G###H##\n";
 	
 }
